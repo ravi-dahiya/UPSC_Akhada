d3070cfd5e4e6d16a9ed766e5d42e2462f6c65c1
ravinderTechpvec18 <ravi.dahiya7987@gmail.com>
2023-11-23 09:13:34 +0530

    initial commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    // distDir: 'build',
+    output: 'export',
+    // distDir: '_static',
+    images: {
+        unoptimized: true
+    }
+}
+
+module.exports = nextConfig
+
---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './globals.css';
+import type { Metadata } from 'next'
+import Header from './layout/Header'
+import Footer from './layout/Footer';
+
+
+export const metadata: Metadata = {
+  title: 'UPSC AKHADA',
+  description: 'Relevant . Quality . Simplified',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  )
+}
