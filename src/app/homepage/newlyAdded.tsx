3177cd56a610611a29b5e3b6a89db087626e9fe4
ravinder <ravi.dahiya7987@gmail.com>
2024-03-02 20:20:05 +0530

    Update newlyAdded.tsx

--- src/app/homepage/newlyAdded.tsx
+++ src/app/homepage/newlyAdded.tsx
@@ -14,44 +14,44 @@ function NewlyAdded() {
             <Row>
                 <Col lg={4} sm={6}>
                     <div className="newly_card">
-                        <h4>Target 550+ in UPSC CSE MAINS- GS and ESSAY</h4>
+                        <h4>UPSC Foundation Course</h4>
                         <p> </p>
-                        <a href='/courses'>Read more</a>
+                         <a href="https://mgxpi.courses.store/353607?utm_source%3Dother%26utm_medium%3Dtutor-course-referral%26utm_campaign%3Dcourse-overview-webapp" target="_blank">Read more</a>
                     </div>
                 </Col>
                 <Col lg={4} sm={6}>
                     <div className="newly_card">
-                        <h4>UPSC GS Paperwise Daily Answer</h4>
+                        <h4>UPSC Mains Daily Answer Writing(GS & ESSAY)</h4>
                         <p> </p>
-                        <a href='/courses'>Read more</a>
+                        <a href="https://mgxpi.courses.store/353417?utm_source%3Dother%26utm_medium%3Dtutor-course-referral%26utm_campaign%3Dcourse-overview-webapp" target="_blank">Read more</a>
                     </div>
                 </Col>
                 <Col lg={4} sm={6}>
                     <div className="newly_card">
-                        <h4>Optional Answer Writing & Mentorship</h4>
+                        <h4>Science & Technology</h4>
                         <p> </p>
-                        <a href='/courses'>Read more</a>
+                        <a href="https://mgxpi.courses.store/399124?utm_source%3Dother%26utm_medium%3Dtutor-course-referral%26utm_campaign%3Dcourse-overview-webapp" target="_blank">Read more</a>
                     </div>
                 </Col>
                 <Col lg={4} sm={6}>
                     <div className="newly_card">
-                        <h4>UPSC Civil Services Special</h4>
+                        <h4>Public Administration Daily Answer Writing</h4>
                         <p> </p>
-                        <a href='/courses'>Read more</a>
+                        <a href="https://mgxpi.courses.store/286727?utm_source%3Dother%26utm_medium%3Dtutor-course-referral%26utm_campaign%3Dcourse-overview-webapp" target="_blank">Read more</a>
                     </div>
                 </Col>
                 <Col lg={4} sm={6}>
                     <div className="newly_card">
-                        <h4>KARAMYOGI - UPPSC Updated </h4>
+                        <h4>Psychology Optional Mentorship and Previous Year Answer Writing </h4>
                         <p>  </p>
-                        <a href='courses'>Read more</a>
+                        <a href="https://mgxpi.courses.store/211279?utm_source%3Dother%26utm_medium%3Dtutor-course-referral%26utm_campaign%3Dcourse-overview-webapp" target="_blank">Read more</a>
                     </div>
                 </Col>
                 <Col lg={4} sm={6}>
                     <div className="newly_card">
-                        <h4>Haryana Civil Services (HCS) 2023</h4>
+                        <h4>PSIR Optional Mentorship and Daily Answer Writing</h4>
                         <p> </p>
-                        <a href='/courses'>Read more</a>
+                        <a href="https://mgxpi.courses.store/271056?utm_source%3Dother%26utm_medium%3Dtutor-course-referral%26utm_campaign%3Dcourse-overview-webapp" target="_blank">Read more</a>
                     </div>
                 </Col>
             </Row>
